49973f77bd046af6dc6300b947c2e95f7fd09ba0
kraudy001 <90103526+kraudy001@users.noreply.github.com>
2022-03-26 22:54:25 +0100

    Models

---
+++ Models/Abilitis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Ghost : IAbility
+    {
+        public void CastAbility(int wericalPlacement, int horisontalPlacement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
