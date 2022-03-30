b0e04f7dfef5176065ab0e73a284761b56a9e1f8
kraudy001 <90103526+kraudy001@users.noreply.github.com>
2022-03-30 10:05:03 +0200

    logic start

--- Logic/Game.cs
+++ Logic/Game.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Models;
+using System;
+using System.Collections.Generic;
 
 namespace Logic
 {
     public class Game
     {
-        
+        List<Motor> Players;
+
+        public void Turn()
+        {
+            foreach (Motor item in Players)
+            {
+                item.move();
+            }
+        }
+
+
+
 
     }
 }
